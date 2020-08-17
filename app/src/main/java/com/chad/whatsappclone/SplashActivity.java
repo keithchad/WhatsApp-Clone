9e0948fb61cbffe50b1637bab56e61e000725344
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-17 14:30:16 +0300

    end-of-splash-screen

--- app/src/main/java/com/chad/whatsappclone/SplashActivity.java
+++ app/src/main/java/com/chad/whatsappclone/SplashActivity.java
@@ -2,7 +2,9 @@ package com.chad.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 3000);
 
 
     }
