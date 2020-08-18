a577155c9e72afc29c57f854de214a96b6b9567d
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-18 11:47:07 +0300

    tab-layout

--- app/src/main/java/com/chad/whatsappclone/MainActivity.java
+++ app/src/main/java/com/chad/whatsappclone/MainActivity.java
@@ -1,14 +1,52 @@
 package com.chad.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    }
+
+    private void setUpWithViewPager(ViewPager viewPager) {
+
+    }
+
+    private static class SectionPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionPagerAdapter(@NonNull FragmentManager manager) {
+            super(manager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+
     }
 }
