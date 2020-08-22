15c41d163d5c9cf1c53c57d0a6d5e9c547976d18
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-22 19:22:18 +0300

    Welcome Activity

---
+++ app/src/main/java/com/chad/whatsappclone/Activity/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.chad.whatsappclone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
