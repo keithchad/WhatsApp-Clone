23c0d7232225238108ecdbff77d4d79d0f25415f
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-27 19:07:44 +0300

    Profile layout

--- app/src/main/java/com/chad/whatsappclone/Activity/ProfileActivity.java
+++ app/src/main/java/com/chad/whatsappclone/Activity/ProfileActivity.java
@@ -1,17 +1,14 @@
-package com.chad.whatsappclone.Settings;
+package com.chad.whatsappclone.Activity;
+
+import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import com.chad.whatsappclone.Activity.ProfileActivity;
 import com.chad.whatsappclone.R;
-import com.chad.whatsappclone.databinding.ActivitySettingsBinding;
+import com.chad.whatsappclone.databinding.ActivityProfileBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,16 +16,16 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class SettingsActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    private ActivitySettingsBinding binding;
+    private ActivityProfileBinding binding;
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firebaseFirestore = FirebaseFirestore.getInstance();
@@ -37,32 +34,26 @@ public class SettingsActivity extends AppCompatActivity {
             getInfo();
         }
 
-        goToProfile();
-
     }
-
-    private void goToProfile() {
-        binding.layoutProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
-            }
-        });
-    }
-
     private void getInfo() {
 
         firebaseFirestore.collection("Users").document(firebaseUser.getUid()).get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
+
                         String userName = String.valueOf(documentSnapshot.get("userName"));
-                        binding.userName.setText(userName);
+                        String phoneNumber = String.valueOf(documentSnapshot.get("userPhone"));
+
+                        binding.userNameText.setText(userName);
+                        binding.phoneNumberText.setText(phoneNumber);
+
+
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.d("TAG", "Error");
+                Toast.makeText(ProfileActivity.this, "Failed to retrieve data!", Toast.LENGTH_SHORT).show();
             }
         });
 
