c09b32aa9432666dbfbbabd41cd4f133289ae089
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-27 13:54:07 +0300

    Sending profile info to firebase

--- app/src/main/java/com/chad/whatsappclone/Authentication/SetUserInfoActivity.java
+++ app/src/main/java/com/chad/whatsappclone/Authentication/SetUserInfoActivity.java
@@ -1,16 +1,84 @@
 package com.chad.whatsappclone.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
+import com.chad.whatsappclone.Activity.MainActivity;
 import com.chad.whatsappclone.R;
+import com.chad.whatsappclone.databinding.ActivitySetUserInfoBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class SetUserInfoActivity extends AppCompatActivity {
+
+    private ActivitySetUserInfoBinding binding;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set_user_info);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_set_user_info);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonClick();
+
+    }
+
+    private void buttonClick() {
+
+        binding.nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(binding.edittextName.getText().toString())) {
+                    Toast.makeText(SetUserInfoActivity.this, "Name Field Cannot be empty", Toast.LENGTH_SHORT).show();
+                }else {
+                    doUpdate();
+                }
+
+            }
+        });
+
+        binding.imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SetUserInfoActivity.this, "Still in development", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void doUpdate() {
+        progressDialog.setMessage("Please Wait");
+        progressDialog.show();
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        if(firebaseUser != null) {
+            firebaseFirestore.collection("Users").document(firebaseUser.getUid()).update("userName", binding.edittextName.getText().toString())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            progressDialog.dismiss();
+                            Toast.makeText(SetUserInfoActivity.this, "Update Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SetUserInfoActivity.this, MainActivity.class));
+                        }
+                    });
+        }else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "You need to login First", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
