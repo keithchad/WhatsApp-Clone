410a85174442f5691ba182890bb96eedf48ebb13
keithchad <63049827+keithchad@users.noreply.github.com>
2020-08-29 13:50:43 +0300

    Bottom Sheet Dialog for gallery and camera

--- app/src/main/java/com/chad/whatsappclone/Activity/ProfileActivity.java
+++ app/src/main/java/com/chad/whatsappclone/Activity/ProfileActivity.java
@@ -2,14 +2,18 @@ package com.chad.whatsappclone.Activity;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -28,9 +32,14 @@ import java.util.Objects;
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
+
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firebaseFirestore;
+
     private BottomSheetDialog bottomSheetDialog;
+
+    private static int REQUEST_CODE_GALLERY = 111;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +48,6 @@ public class ProfileActivity extends AppCompatActivity {
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firebaseFirestore = FirebaseFirestore.getInstance();
-        bottomSheetDialog = new BottomSheetDialog(getApplicationContext());
         if(firebaseUser != null) {
             getInfo();
         }
@@ -53,25 +61,37 @@ public class ProfileActivity extends AppCompatActivity {
         binding.fabCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                @SuppressLint("InflateParams") View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_sheet_pick, null);
-
-
-                bottomSheetDialog.setContentView(view);
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    Objects.requireNonNull(bottomSheetDialog.getWindow()).addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            }
-
-                bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                    public void onDismiss(DialogInterface dialog) {
-
+                bottomSheetDialog = new BottomSheetDialog(
+                        ProfileActivity.this, R.style.BottomSheetDialogTheme
+                );
+                View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_sheet_pick, (LinearLayout)findViewById(R.id.bottomSheetContainer));
+                view.findViewById(R.id.layout_gallery).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       openGallery();
+                        bottomSheetDialog.dismiss();
                     }
                 });
-
+                view.findViewById(R.id.layout_camera).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ProfileActivity.this, "Camera Clicked", Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(view);
                 bottomSheetDialog.show();
             }
         });
+
+    }
+
+    private void openGallery() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "select image"), REQUEST_CODE_GALLERY);
 
     }
 
@@ -96,6 +116,16 @@ public class ProfileActivity extends AppCompatActivity {
                 Toast.makeText(ProfileActivity.this, "Failed to retrieve data!", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+        }
 
     }
 }
