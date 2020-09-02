8a26b74aa7c0e9538fc3e50b807882686da37f02
keithchad <63049827+keithchad@users.noreply.github.com>
2020-09-02 21:13:06 +0300

    Showing profile image in Status

--- app/src/main/java/com/chad/whatsappclone/Fragments/StatusFragment.java
+++ app/src/main/java/com/chad/whatsappclone/Fragments/StatusFragment.java
@@ -2,24 +2,64 @@ package com.chad.whatsappclone.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.chad.whatsappclone.R;
+import com.chad.whatsappclone.databinding.FragmentStatusBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class StatusFragment extends Fragment {
 
-    public StatusFragment() {
-        // Required empty public constructor
-    }
+    public StatusFragment() { }
+
+    FragmentStatusBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_status, container, false);
+
+        getProfile();
+
+        return  binding.getRoot();
+    }
+
+    private void getProfile() {
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+        if (firebaseUser != null) {
+            firebaseFirestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                    String imageProfile = String.valueOf(documentSnapshot.get("imageProfile"));
+
+                    Glide.with(getContext()).load(imageProfile).into(binding.profileImage);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), "Something Went Wrong!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
